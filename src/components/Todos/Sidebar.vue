47b6dfa5b44abd32795dec45561b1c56340cf75d
KhodeMamalizz <96520391+KhodeMamalizz@users.noreply.github.com>
2022-09-15 13:53:08 +0430

    - new changes

--- src/components/Todos/Sidebar.vue
+++ src/components/Todos/Sidebar.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="sidebars hidden md:block w-full h-full bg-[#ffffff16] text-[#fff] rounded-md p-5">
-    <ul>
-        <li>test1</li>
-        <li>test2</li>
-        <li>test3</li>
-        <li>test4</li>
-    </ul>
   </div>
 </template>
 
